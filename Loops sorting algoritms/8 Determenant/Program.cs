3843affdd82132d2f61aa23cb04e7cc52b24556a
Horfik <41876591+Horfik@users.noreply.github.com>
2018-11-07 16:18:41 +0300

    v1

---
+++ Loops sorting algoritms/8 Determenant/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Determenant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] matrix = new double[20, 20];
+            Random rnd = new Random();
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    matrix[i, j] = rnd.Next(10);
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            double k;
+            for (int l = 0; l < matrix.GetLength(0) - 1; l++)
+            {
+                for (int i = l; i < matrix.GetLength(0) - 1; i++)
+                {
+                    k = matrix[i + 1, l] / matrix[l, l];
+                    for (int j = l; j < matrix.GetLength(1); j++)
+                    {
+                        matrix[i + 1, j] = matrix[i + 1, j] - k * matrix[l, j];
+                    }
+                }
+            }
+
+            Console.WriteLine();
+
+            double det = 1;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                det *= matrix[i, i];
+            }
+            Console.WriteLine("det = " + det);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Loops sorting algoritms/8 Determenant/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("8 Determenant")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("8 Determenant")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("e1d3b169-37ee-410e-b379-7c07a9e2441b")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
